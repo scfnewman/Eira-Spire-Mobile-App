e3d361ff115019568426ecf7269897c44822757d
Simon Newman <simon@ineedsurgery.com>
2020-02-17 10:28:43 +0000

    Added new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{
+		path: 'home',
+		loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+	},
+	{
+		path: 'characters',
+		loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersPageModule)
+	},
+	{
+		path: 'history',
+		loadChildren: () => import('./pages/history/history.module').then(m => m.HistoryPageModule)
+	},
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+	],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
