c98094edb628c71d5acd7c2e77d9075b2efea226
Simon Newman <simon@ineedsurgery.com>
2020-02-17 10:48:19 +0000

    Added code to open character modal

--- src/app/pages/characters/characters.page.ts
+++ src/app/pages/characters/characters.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+import { CharacterModal } from '../../components/character/character.component'
 
 @Component({
-  selector: 'app-characters',
-  templateUrl: './characters.page.html',
-  styleUrls: ['./characters.page.scss'],
+	selector: 'app-characters',
+	templateUrl: './characters.page.html',
+	styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
 
-  constructor() { }
+	constructor(
+		public _ModalController: ModalController
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	async PresentModal() {
+		const Modal = await this._ModalController.create({
+			component: CharacterModal
+		});
+		return Modal.present();
+	}
 
 }
